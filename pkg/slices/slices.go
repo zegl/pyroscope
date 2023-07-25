623c9c2367b3a2839177c78034bc21a7722363c6
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2023-07-25 16:27:42 +0800

    Make RemoveInPlace to swap elements

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -4,11 +4,9 @@ package slices
 // Does not allocate a new slice.
 func RemoveInPlace[T any](collection []T, predicate func(T, int) bool) []T {
 	i := 0
-	var t T
 	for j, x := range collection {
 		if !predicate(x, j) {
-			collection[j] = t
-			collection[i] = x
+			collection[j], collection[i] = collection[i], collection[j]
 			i++
 		}
 	}
